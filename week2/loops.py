695cff43873fc718c8181944b55906c61f1a84aa
Nick Volynkin <nick.volynkin@gmail.com>
2018-11-27 09:50:52 +0700

    fix sorting and names, update docstrings

--- week2/loops.py
+++ week2/loops.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import itertools
 import random
 import statistics
-import itertools
 
 
 def loops1():
@@ -19,12 +19,18 @@ def one_char_by_line(input_string):
     return '\n'.join(input_string.split())
 
 
-def generate_random_marks(number):
-    def rnd_class():
+def generate_random_grades(number):
+    """
+    Generate a random student with a class label and a list of grades
+    :param number:
+    :return:
+    """
+
+    def rnd_class_label():
         return str(random.randint(1, 11)) + \
                random.choice(['а', 'б', 'в', 'г', 'д'])
 
-    def rnd_marks():
+    def rnd_grades():
         """
         3 to 7 marks, each varies from 2 to 5.
         :return:
@@ -33,30 +39,50 @@ def generate_random_marks(number):
 
     for i in range(number):
         yield {
-            'school_class': rnd_class(),
-            'scores': rnd_marks()
+            'school_class': rnd_class_label(),
+            'scores': rnd_grades()
         }
 
 
-def get_avg_school_mark(students):
+def get_avg_school_grade(students):
     """
-    get average school mark
-    :param marks: [{'school_class': '', scores}]
+    Get grade point average for all students
+    :param students: [{'school_class': 'str', scores: [int]}, ...]
     :return:
     """
     chain = itertools.chain.from_iterable([student['scores'] for student in students])
     return statistics.mean(chain)
 
 
-def get_avg_class_mark(students, class_name):
-    class_marks = filter(lambda student: student['school_class'] == class_name, students)
+def get_avg_class_grade(students, class_label):
+    """
+    Get grade point average for a given class
+    :param students: [{'school_class': 'str', scores: [int]}, ...]
+    :param class_label:
+    :return:
+    """
+    class_marks = filter(lambda student: student['school_class'] == class_label, students)
     chain = itertools.chain.from_iterable([student['scores'] for student in class_marks])
     return statistics.mean(chain)
 
 
-def get_all_classes(students):
-    def sorter(name):
-        return int(name[:-1])
+def list_class_labels(students):
+    """
+    Get a list of unique class labels, sorted 1st to 11th
+    :param students:
+    :return:
+    """
+
+    def sorter(label):
+        """
+        add leading zero to 1-digit class labels
+        :param label:
+        :return:
+        """
+        if len(label) == 2:
+            return '0' + label
+        else:
+            return label
 
     classes_set = set([student['school_class'] for student in students])
     classes_list = sorted(list(classes_set), key=sorter)
@@ -64,14 +90,15 @@ def get_all_classes(students):
 
 
 def main():
-    students = list(generate_random_marks(1000))
+    students = list(generate_random_grades(1000))
     print('{} students'.format(len(students)))
-    avg_school = get_avg_school_mark(students)
+    avg_school = get_avg_school_grade(students)
     print('average school mark: {}'.format(avg_school))
     print('average class marks:')
 
-    for class_ in get_all_classes(students):
-        print('\t{}: {}'.format(class_, get_avg_class_mark(students, class_)))
+    for class_ in list_class_labels(students):
+        print('\t{}: {}'.format(class_, get_avg_class_grade(students, class_)))
+
 
 if __name__ == '__main__':
     main()
