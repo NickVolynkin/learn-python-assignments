7af84c27593a93c4b11f42f2a3b04bd762ddffe0
Nick Volynkin <nick.volynkin@gmail.com>
2018-11-29 15:12:49 +0700

    separate processing input and performing output

--- week2/loops2.py
+++ week2/loops2.py
@@ -2,28 +2,46 @@ import datetime
 
 
 def ask_user():
+    while True:
+        try:
+            user_input = input("\nКак дела?\n").lower()
+            answer = get_answer(user_input)
+        except (KeyboardInterrupt, EOFError) as exc:
+            # Ctrl+C is KeyboardInterrupt,
+            # Ctrl+D is EOFError
+            answer = get_answer(type(exc))
+
+        if type(answer) is list:
+            for a in answer:
+                process_answer(a)
+        else:
+            process_answer(answer)
+
+
+def process_answer(answer):
+    if callable(answer):
+        print(answer())
+    else:
+        print(answer)
+
+
+def get_answer(user_input):
     known_questions = {
         "время": time,
         "дата": date,
-        "learn": "python"
-
+        "learn": "python",
+        "хорошо": ['Пока!', quit_program],
+        KeyboardInterrupt: ['Пока!', quit_program],
+        EOFError: ['Пока!', quit_program],
+        None: '¯\_(ツ)_/¯'
     }
-    while True:
-        try:
-            user_input = input("Как дела?\n").lower()
-            print('({})'.format(user_input))
-            if user_input == "хорошо":
-                print("Пока!")
-                break
-            elif user_input in known_questions:
-                answer = known_questions[user_input]
-                if callable(answer):
-                    print(answer())
-                else:
-                    print(answer)
-        except KeyboardInterrupt:
-            print("\nПока!")
-            break
+
+    if user_input in known_questions:
+        answer = known_questions[user_input]
+    else:
+        answer = known_questions[None]
+
+    return answer
 
 
 def time():
@@ -32,6 +50,10 @@ def time():
 
 def date():
     return datetime.datetime.now().date()
+
+
+def quit_program():
+    exit(0)
 
 
 if __name__ == '__main__':
